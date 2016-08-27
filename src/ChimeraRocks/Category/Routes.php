cc9b6cb357880463173b044a0c6d4b2a11ffa251
chimerarocks <joaopedrodslv@gmail.com>
2016-08-27 11:25:38 -0300

    adding user package

--- src/ChimeraRocks/Category/Routes.php
+++ src/ChimeraRocks/Category/Routes.php
@@ -4,7 +4,7 @@ Route::group([
 	'prefix' => 'admin/categories', 
 	'namespace' => 'ChimeraRocks\Category\Controllers',
 	'as' => 'admin.categories.',
-	'middleware' => ['web']
+	'middleware' => ['web', 'auth']
 	], function() {
 	Route::get('/', ['uses' => 'AdminCategoryController@index', 'as' => 'index']);
 	Route::get('/create', ['uses' => 'AdminCategoryController@create', 'as' => 'create']);
